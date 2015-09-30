46224e32929ec38f1520fff39b136e25c2cf669d
leeper <thosjleeper@gmail.com>
2015-09-30 16:20:35 +0100

    document s3save/s3load

--- R/saveload.r
+++ R/saveload.r
@@ -1,4 +1,22 @@
-s3save <- function(..., bucket, object, opts) {
+#' @title Save to S3
+#'
+#' @description Save an R object to S3 ala \code{\link[base]{save}}.
+#' 
+#' @details Save an R object directly to an S3 bucket rather than a local directory.
+#' 
+#' @param ... Additional arguments passed to \code{\link{s3HTTP}}.
+#' @param bucket A character string containing the name of the bucket you want 
+#' to delete an object from.
+#' @param object A character string containing the name of the object.
+#' @param opts An optional list of options to be passed to \code{\link{postobject}}.
+#'
+#' @return If successful, \code{NULL}, else the contents of the API error.
+#' 
+#' @keywords bucket, object
+#' @seealso \code{\link{postobject}}, \code{\link{s3load}}
+#' @export
+
+s3save <- function(..., bucket, object, opts = list()) {
     tmp <- tempfile(fileext = ".Rdata")
     on.exit(unlink(tmp))
     save(..., file = tmp)
@@ -8,6 +26,25 @@ s3save <- function(..., bucket, object, opts) {
     else
         return(invisible())
 }
+
+#' @title Load from S3
+#'
+#' @description Load an R object from S3 ala \code{\link[base]{load}}.
+#' 
+#' @details Load an R object directly from an S3 bucket rather than a local directory.
+#' 
+#' @param bucket A character string containing the name of the bucket you want 
+#' to delete an object from.
+#' @param object A character string containing the name of the object.
+#' @param opts An optional list of options to be passed to \code{\link{getobject}}.
+#' @param envir An R environment, by default the parent environment, into which the R 
+#' object(s) should be loaded
+#'
+#' @return If successful, \code{NULL}, else the contents of the API error.
+#' 
+#' @keywords bucket, object
+#' @seealso \code{\link{getobject}}, \code{\link{s3save}}
+#' @export
 
 s3load <- function(bucket, object, opts, envir = parent.frame()) {
     tmp <- tempfile(fileext = ".Rdata")
